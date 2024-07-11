02ba945a4234346299b57b621b28d1024ba741df
martines3000 <domajnko.martin@gmail.com>
2024-07-11 10:49:20 +0200

    chore: update settings json

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { createServer } = require('http');
-const { parse } = require('url');
+const { createServer } = require('node:http');
+const { parse } = require('node:url');
 
 const next = require('next');
 const dev = false;
@@ -19,17 +19,16 @@ app.prepare().then(() => {
       const parsedUrl = parse(req.url, true);
       await handle(req, res, parsedUrl);
     } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
     }
   })
     .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
+      console.error(err);
+      process.exit(1);
     })
     .listen(port, () => {
       console.log(`> Ready on http://${hostname}:${port}`);
-    })
-
-})
+    });
+});
